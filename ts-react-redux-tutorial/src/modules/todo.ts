06639d79ef8e9295b4aa46f58713ec72013642b2
sorikiki <sorikiki98@naver.com>
2020-08-26 14:52:17 +0900

    todo reducer

---
+++ ts-react-redux-tutorial/src/modules/todo.ts
@@ -0,0 +1,56 @@
+const INSERT = 'todo/INSERT' as const;
+const DELETE = 'todo/DELETE' as const;
+const TOGGLE = 'todo/TOGGLE' as const;
+
+export const insert_todo = (text : string) => ({
+    type: INSERT,
+    payload: text,
+});
+
+export const delete_todo = (id: number) => ({
+    type: DELETE,
+    payload: id,
+});
+
+export const toggle_todo = (id: number) => ({
+    type: TOGGLE,
+    payload: id,
+})
+
+type TodosAction =
+  | ReturnType<typeof insert_todo>
+  | ReturnType<typeof delete_todo>
+  | ReturnType<typeof toggle_todo>;
+
+type Todo = {
+    id: number,
+    text: string,
+    done: boolean,
+};
+
+type TodoState = Todo[];
+
+const initialState : TodoState = [
+    {
+        id: 1,
+        text: 'text',
+        done: false,
+    }
+];
+
+let id = 1;
+
+const todoReducer = (state: TodoState = initialState, action: TodosAction) => {
+    switch(action.type) {
+        case INSERT:
+            return state.concat({id: id+1, text: action.payload, done: false});
+        case DELETE:
+            return state.filter(todo => todo.id !== action.payload);
+        case TOGGLE:
+            return state.filter(todo => todo.id === action.payload).map(todo => todo.done = !todo.done);
+        default:
+            return state;
+    }
+};
+
+export default todoReducer;
