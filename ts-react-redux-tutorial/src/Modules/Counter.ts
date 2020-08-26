af5a5df899ed69c3ca8f25eddb7c5e6a78a8b99a
sorikiki <sorikiki98@naver.com>
2020-08-26 12:08:04 +0900

    counter reducer

---
+++ ts-react-redux-tutorial/src/Modules/Counter.ts
@@ -0,0 +1,37 @@
+export const increase = () => {
+    type: 'INCREASE';
+}
+
+export const decrease = () => {
+    type: 'DECREASE';
+}
+
+export const increase_by = (diff : number) => {
+    type: 'INCREASE_BY';
+    payload: diff;
+}
+
+type CounterAction = { type: 'INCREASE' } | { type: 'DECREASE' } | { type: 'INCREASE_BY', payload: number };
+
+type CounterState = {
+    count: number
+}
+
+const initialState : CounterState = {
+    count: 0
+}
+
+const counterReducer = (state: CounterState = initialState, action : CounterAction) : CounterState => {
+    switch (action.type) {
+        case 'INCREASE':
+            return { count: state.count + 1 };
+          case 'DECREASE':
+            return { count: state.count - 1 };
+          case 'INCREASE_BY':
+            return { count: state.count + action.payload };
+          default:
+            return state;
+        }
+      }
+      
+export default counterReducer;
